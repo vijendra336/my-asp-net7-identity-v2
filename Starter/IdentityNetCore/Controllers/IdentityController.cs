11e57e307e91f71247deef43369775b3d4508c86
vijendra336 <39852345+vijendra336@users.noreply.github.com>
2023-10-23 13:33:52 +0530

    S2 C8 Signup2 Signup View with validatation and Signup Post Action method.

--- Starter/IdentityNetCore/Controllers/IdentityController.cs
+++ Starter/IdentityNetCore/Controllers/IdentityController.cs
@@ -6,7 +6,12 @@ namespace IdentityNetCore.Controllers
 {
     public class IdentityController : Controller
     {
-        
+        private readonly UserManager<IdentityUser> userManager;
+
+        public IdentityController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
         public async Task<IActionResult> Signup()
         {
             var model = new SignupViewModel();
@@ -17,7 +22,29 @@ namespace IdentityNetCore.Controllers
         [HttpPost]
         public async Task<IActionResult> Signup(SignupViewModel model)
         {
+            if(ModelState.IsValid)
+            {
+               var checkUserEmail = await userManager.FindByEmailAsync(model.Email);
 
+               if(checkUserEmail != null)
+                {
+                    var user = new IdentityUser
+                    {
+                        Email = model.Email,
+                        UserName = model.Email,
+                    };
+
+                    var result = await userManager.CreateAsync(user, model.Password);
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Signin", "Identity");
+                    }
+
+                    ModelState.AddModelError("Signup", string.Join("",result.Errors.Select(s=>s.Description)));
+                    return View(model);
+                }
+            }
             return View(model);
         }
 
