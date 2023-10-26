4bdc2e5a13886a0c29f721f10c67aa94cca83552
vijendra336 <39852345+vijendra336@users.noreply.github.com>
2023-10-26 14:10:01 +0530

    S6 Common Security Risks - Fresh MVC application without changes.

---
+++ CommonSecurityAttacks/StudentEnroll/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentEnroll.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
